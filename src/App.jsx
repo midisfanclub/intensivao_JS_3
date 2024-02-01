2c19a4a4889adc84957dc244268a53c6ac2f30b6
AlicePlentz <alicecplentz@gmail.com>
2024-02-01 16:40:21 -0300

    Versão final - Cardápi

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"; //para usar a variavel de estado (useState())
+
 import Hashtaurante from "./assets/hashtaurante.webp";
 import './App.css'; {/*para importar ss*/}
 import { Navegacao } from "./Navegacao";
@@ -6,13 +8,15 @@ import { pratosPrincipais, sobremesas, bebidas } from "./cardapio";
 
 
 export function App() {
-  
+  const pagina_menu = [pratosPrincipais, sobremesas, bebidas];
+  const [pagina_selecionada, atualizar_pagina_selecionada] = useState(0); //variavel de estado
+
   return <>
           <img src={Hashtaurante} alt="" className="capa" />
           {/*className --> para puxar classe do css*/}
-          <Navegacao /> {/*Navegacao contem os tres botões*/}
+          <Navegacao atualizar_pagina_selecionada={atualizar_pagina_selecionada}/> {/*Navegacao contem os tres botões*/}
           <div className="menu">
-            {pratosPrincipais.map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
+            {pagina_menu[pagina_selecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
 
           </div>
         </>
--- src/Navegacao.jsx
+++ src/Navegacao.jsx
@@ -1,11 +1,11 @@
-export function Navegacao() {
+export function Navegacao(props) {
     return <div className="navegacao">{/*className --> para puxar classe do css*/}
 
-        <input type="radio" name="opcao-pagina" id="pagina-0" defaultChecked/>
+        <input type="radio" name="opcao-pagina" id="pagina-0" defaultChecked onClick={() => props.atualizar_pagina_selecionada(0)}/>
         <label htmlFor="pagina-0">Pratos Principais</label>
-        <input type="radio" name="opcao-pagina" id="pagina-1" />
+        <input type="radio" name="opcao-pagina" id="pagina-1" onClick={() => props.atualizar_pagina_selecionada(1)} />
         <label htmlFor="pagina-1">Sobremesas</label>
-        <input type="radio" name="opcao-pagina" id="pagina-2" />
+        <input type="radio" name="opcao-pagina" id="pagina-2" onClick={() => props.atualizar_pagina_selecionada(2)} />
         <label htmlFor="pagina-2">Bebidas</label>
     </div>
 }
